e27091bcbf059146f7c37052c647e29bcd3401bd
rushinandigam <gnyana rushi>
2025-08-21 12:11:03 +0530

    register js changes

--- day3/script.js
+++ day3/script.js
@@ -1,8 +1,4 @@
 let firstname = document.getElementById("firstname");
 let lastname = document.getElementById("lastname");
 let email = document.getElementById("email");
-let gender = document.getElementById("gender");
-let form = document.getElementById("formapp");
-form.addEventListener("keyup" , ()=>{
-    console.log(firstname.value , lastname.value , email.value , gender );
-})
+let gender = document.getElementsByName("gender");
